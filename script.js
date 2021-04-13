1e77c53c51c2c9b6598f1be45d2f8cb342aa19d5
catherinesyeh <cy3@williams.edu>
2021-04-13 15:47:33 -0700

    prints stats now!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+brython(); // initialize again just in case
+
 var header = document.getElementById("header");
 
 var topButton = document.getElementById("back-to-top");
@@ -29,11 +31,46 @@ function backToTop() { // deal with back to top button
     legend.classList.toggle("active", window.scrollY >= offset);
 }
 
+var pos_stats = document.getElementById("pos-stats");
+var align_stats = document.getElementById("align-stats");
+
+function addStat(output, key, val) {
+    s = document.createElement('p');
+    s.innerHTML = "<i><b>" + key + ":</b></i> " + val.toFixed(1);
+    output.appendChild(s);
+}
+
+function printStats(data, key) {
+    var output = pos_stats;
+    if (key == "s") {
+        output = align_stats;
+    }
+    output.innerHTML = "";
+
+    var min = ss.min(data);
+    addStat(output, "min", min);
+    var max = ss.max(data);
+    addStat(output, "max", max);
+    var range = max - min;
+    addStat(output, "range", range);
+    var median = ss.max(data);
+    addStat(output, "median", median);
+    var mean = ss.mean(data);
+    addStat(output, "mean", mean);
+    var sd = ss.standardDeviation(data);
+    addStat(output, "sd", sd);
+}
+
 $('#submit-button').click(function () {
     $('#spin').addClass("visible");
     setTimeout(function () {
-        var res = sw();
+        var res_all = sw();
+        var res = res_all[0];
         if (res) {
+            var pos = res_all[1];
+            var scores = res_all[2];
+            printStats(pos, "p");
+            printStats(scores, "s");
             $('#see-matches').addClass("active");
         }
         $('#spin').removeClass("visible");
